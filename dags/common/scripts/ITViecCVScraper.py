6ae9672ba006b2c7e234547e744aaecea71ce34d
kimhung1100 <hungbk1100@gmail.com>
2023-07-11 17:08:59 +0700

    create .env

---
+++ dags/common/scripts/ITViecCVScraper.py
@@ -0,0 +1,203 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import urllib.parse
+import unicodedata
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+
+
+class ITViecCVScraper:
+    
+    
+    def __init__(self):
+        self.session = requests.Session()
+        self.s3_hook = S3Hook(aws_conn_id='aws_default')
+        self.base_url = 'https://itviec.com'
+        
+        
+    
+    def login(self, login_url, username, password):
+        """
+        Logs in to the ITviec website.
+
+        Input:
+        - login_url: URL of the login page.
+        - username: Username or email.
+        - password: Password for the account.
+
+        Logic:
+        - Sends a GET request to the login page to fetch the authenticity_token.
+        - Extracts the authenticity_token from the response HTML.
+        - Sends a POST request with the login credentials and authenticity_token.
+        - Checks if the login was successful based on the response status code.
+
+        Output:
+        - Returns True if the login was successful, False otherwise.
+        """
+        r = self.session.get(login_url)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        authenticity_token = soup.find('input', attrs={'name': 'authenticity_token'})['value']
+
+        payload = {
+            'authenticity_token': authenticity_token,
+            'customer[email]': username,
+            'customer[password]': password,
+            'customer[remember_me]': '0'
+        }
+
+        login_response = self.session.post(login_url, data=payload)
+
+        if login_response.status_code == 200:
+            print("Login successful.")
+            return True
+        else:
+            print("Login failed.")
+            return False
+    
+    def download_cv(self, cv_url, folder_name, name, email, job, submitted_date, CVs_path):
+        """
+        Downloads a CV file.
+
+        Input:
+        - cv_url: URL of the CV download page.
+        - folder_name: Name of the folder to save the CV file.
+        - name: Name of the applicant.
+        - email: Email of the applicant.
+        - job: Job title the applicant applied for.
+        - submitted_date: Date the application was submitted.
+
+        Logic:
+        - Sends a GET request to the CV download page to fetch the download link.
+        - Extracts the download link from the response HTML.
+        - Sends a GET request to download the CV file.
+        - Saves the CV file in the specified folder in S3.
+        - Renames the CV file with a formatted filename.
+
+        Output:
+        - None
+        """
+        cv_response = self.session.get(cv_url, allow_redirects=False)
+        cv_soup = BeautifulSoup(cv_response.content, 'html.parser')
+        a_tag = cv_soup.find('a', attrs={'class': 'btn btn-danger'})['href']
+        cv_response = self.session.get(a_tag)
+
+        filename = a_tag.split('/')[-1]
+        file_path = os.path.join(folder_name, filename)
+
+        with open(file_path, 'wb') as f:
+            f.write(cv_response.content)
+
+        print(f"Downloaded file for {name} - {email}")
+
+        new_filename = f"{unicodedata.normalize('NFKD', name).encode('ASCII', 'ignore').decode()}_{email}_{job}_{submitted_date}.pdf"
+        new_filename = new_filename.replace("/", "_")
+        decoded_new_filename = urllib.parse.unquote(new_filename)
+        new_file_path = os.path.join(folder_name, decoded_new_filename)
+
+        os.rename(file_path, new_file_path)
+
+        # Upload the CV file to S3
+        s3_key = os.path.join(CVs_path, os.path.basename(new_file_path))
+        if self.s3_hook.check_for_key(s3_key, bucket_name='rockship-recruitment-process'):
+            print(f"File {s3_key} already exists. Skipping download.")
+            return  # Skip the download and continue with the next CV
+    
+        self.s3_hook.load_file(
+            filename=new_file_path,
+            key=s3_key,
+            bucket_name='rockship-recruitment-process',
+        )
+
+    def get_max_page(self, applications_url):
+        """
+        Retrieves the maximum page number from the job applications.
+
+        Input:
+        - applications_url: URL of the job applications page.
+
+        Logic:
+        - Sends a GET request to the job applications page.
+        - Parses the HTML response to find the pagination element.
+        - Extracts the maximum page number from the pagination links.
+
+        Output:
+        - Returns the maximum page number as an integer.
+        """
+        applications_response = self.session.get(applications_url)
+        soup = BeautifulSoup(applications_response.content, 'html.parser')
+        pagination = soup.find('ul', attrs={'class': 'pagination'})
+
+        if not pagination:
+            return 1
+
+        last_page = 1
+        for page_link in pagination.find_all('a'):
+            page_number = int(page_link.text)
+            last_page = max(last_page, page_number)
+
+        return last_page
+    
+    def download_all_cvs(self, applications_url, per_page=100, CVs_path='CVs'):
+        """
+        Downloads all CVs from the job applications.
+
+        Input:
+        - applications_url: URL of the job applications page.
+        - per_page: Number of applications to display per page (default is 100).
+
+        Logic:
+        - Retrieves the maximum page number using the get_max_page method.
+        - Iterates over each page and sends a GET request to the page URL.
+        - Parses the HTML response to find the table rows.
+        - Extracts the applicant information and CV download URL from each row.
+        - Creates a folder to save the CV files if it doesn't exist.
+        - Calls the download_cv method to download and save each CV to S3.
+
+        Output:
+        - None
+        """
+        max_page = self.get_max_page(applications_url)
+
+        for page in range(1, max_page + 1):
+            page_url = f"{applications_url}?page={page}&per={per_page}"
+            applications_response = self.session.get(page_url)
+            soup = BeautifulSoup(applications_response.content, 'html.parser')
+            rows = soup.find_all('tr')
+
+            for row in rows:
+                if row.find('th'):
+                    continue
+
+                cells = row.find_all('td')
+                name = cells[0].text.strip()
+                email = cells[1].text.strip()
+                job = cells[2].text.strip()
+                submitted_date = cells[3].text.strip()
+                cv_url = self.base_url + cells[4].find('a')['href']
+
+                folder_name = 'CV'
+                if not os.path.exists(folder_name):
+                    os.makedirs(folder_name)
+
+                self.download_cv(cv_url, folder_name, name, email, job, submitted_date, CVs_path)
+                
+
+    def get_cv_url(self, cv_page_url):
+        """
+        Retrieves the CV download URL from the CV page.
+
+        Input:
+        - cv_page_url: URL of the CV page.
+
+        Logic:
+        - Sends a GET request to the CV page.
+        - Parses the HTML response to find the CV download URL.
+
+        Output:
+        - Returns the CV download URL as a string or None if not found.
+        """
+        cv_page_response = self.session.get(cv_page_url)
+        cv_soup = BeautifulSoup(cv_page_response.content, 'html.parser')
+        cv_download_link = cv_soup.find('a', attrs={'class': 'btn btn-danger'})['href'] if cv_soup.find('a', attrs={'class': 'btn btn-danger'}) else None
+        return cv_download_link
+
---
+++ dags/recruit_process/RS_recruit.py
@@ -0,0 +1,113 @@
+import os
+import sys
+from airflow import DAG
+from airflow.decorators import task
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from pendulum import datetime
+import logging
+
+# Add the parent directory to sys.path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from common.scripts.ITViecCVScraper import ITViecCVScraper
+from common.scripts.pdf_converter import pdf_converter
+from dotenv import load_dotenv
+
+
+
+# Load environment variables from .env file
+load_dotenv()
+
+AWS_CONN_ID = os.getenv('AWS_CONN_ID')
+bucket_name = os.getenv('BUCKET_NAME')
+CVs_path = os.getenv('CVs_path')
+CVs_text_path = os.getenv('CVs_text_path')
+
+default_args = {
+    "retries": 2,
+}
+
+@task
+def crawl_cvs_task():
+    # Task 0: Crawl all CVs from itviec.com
+    # ITviec.com login info
+    # later save in airflow variable for more security
+    login_url = os.getenv('ITVIEC_LOGIN_URL')
+    applications_url = os.getenv('ITVIEC_APPLICATIONS_URL')
+    username = os.getenv('ITVIEC_USERNAME')
+    password = os.getenv('ITVIEC_PASSWORD')
+
+    downloader = ITViecCVScraper()
+    if downloader.login(login_url, username, password):
+        downloader.download_all_cvs(applications_url, per_page=10)
+
+    # Check if CVs have been downloaded
+    if os.path.exists("CV"):
+        cv_files = os.listdir("CV")
+        cv_files = cv_files[:10]  # Select only the first 10 CVs for testing
+        for filename in cv_files:
+            if filename.endswith(".pdf"):
+                pdf_file_path = os.path.join("CV", filename)
+                print(f"Downloaded {pdf_file_path}")
+
+@task
+def download_pdf_file(s3_file):
+    # Task 1: Download the PDF file from S3 to local
+    s3_hook = S3Hook(aws_conn_id=AWS_CONN_ID)
+    local_pdf_path = f"/tmp/{os.path.basename(s3_file)}"
+    s3_hook.download_file(bucket_name, s3_file, local_pdf_path)
+    return local_pdf_path
+
+@task
+def convert_pdf_to_text_task(pdf_file_path):
+    # Task 2: Convert PDF to text using pdfplumber
+    text = convert_pdf_to_text(pdf_file_path)
+    return text
+
+@task
+def upload_text_file(text, s3_file):
+    # Task 3: Upload the text file to S3
+    text_file_name = os.path.splitext(os.path.basename(s3_file))[0] + ".txt"
+    local_text_path = f"/tmp/{text_file_name}"
+
+    # Save the text to a local file
+    with open(local_text_path, 'w') as text_file:
+        text_file.write(text)
+
+    # Upload the text file to S3
+    s3_hook = S3Hook(aws_conn_id=AWS_CONN_ID)
+    s3_hook.upload_file(local_text_path, bucket_name, s3_file)
+    return local_text_path
+
+@task
+def remove_local_files(pdf_file_path, text_file_path):
+    # Task 4: Remove the local PDF and text files
+    os.remove(pdf_file_path)
+    os.remove(text_file_path)
+
+with DAG(
+    'cv_processing_dag',
+    default_args=default_args,
+    schedule_interval=None,
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    tags=["Rockship Recruitment Process"]
+) as dag:
+    # Task 0: Crawl all CVs from itviec.com
+    crawl_cvs = crawl_cvs_task()
+
+    # Task 1: Retrieve and convert PDFs to text
+    cv_files = S3Hook(aws_conn_id=AWS_CONN_ID).list_keys(bucket_name=bucket_name, prefix=CVs_path)
+    retrieve_and_convert_pdfs = []
+    for cv_file in cv_files:
+        if cv_file.lower().endswith(".pdf"):
+            pdf_file_path = download_pdf_file(cv_file)
+            text = convert_pdf_to_text_task(pdf_file_path)
+            text_file_path = upload_text_file(text, cv_file)
+            logging.info(f"Converted PDF to text: s3://{bucket_name}/{cv_file}")
+            retrieve_and_convert_pdfs.append((pdf_file_path, text_file_path))
+    
+    # Set task dependencies
+    crawl_cvs >> retrieve_and_convert_pdfs >> remove_local_files
